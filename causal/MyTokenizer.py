6fea24a0557fb750e3b769615fe38c830c9eddf8
whusjj <139863151+whusjj@users.noreply.github.com>
2024-04-28 00:12:23 +0800

    预训练

---
+++ causal/MyTokenizer.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+from pathlib import Path
+from tokenizers import ByteLevelBPETokenizer
+from tokenizers.processors import BertProcessing
+import torch
+import pdb
+class EsperantoDataset(Dataset):
+    def __init__(self, evaluate: bool = False, batch_size=32):
+        self.batch_size = batch_size
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.tokenizer = ByteLevelBPETokenizer(
+            "esperberto-vocab.json",
+            "esperberto-merges.txt",
+        )
+        self.tokenizer._tokenizer.post_processor = BertProcessing(
+            ("</s>", self.tokenizer.token_to_id("</s>")),
+            ("<s>", self.tokenizer.token_to_id("<s>")),
+        )
+        self.tokenizer.enable_truncation(max_length=128)
+
+
+    def tokenizer_node(self, graph_data):
+        # 存储编码后数据的列表
+        encoded_sequence = [torch.tensor(self.tokenizer.encode(str(seq), add_special_tokens=True).ids).to(self.device) for seq in graph_data]
+        max_length = max(len(tensor) for tensor in encoded_sequence)
+        padded_tensor_list = [torch.cat((tensor.to(self.device), torch.ones(max_length - len(tensor), dtype=torch.long).to(self.device))) for tensor in encoded_sequence]
+        # 对 graph_data 中的每个项进行处理
+        return padded_tensor_list
