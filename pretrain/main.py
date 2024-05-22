ddd7bb9754f25a11f58b289a657887feb61e8f89
Marvinmw <weima93@gmail.com>
2024-05-22 11:36:06 +0000

    python code

--- pretrain/main.py
+++ pretrain/main.py
@@ -7,7 +7,11 @@ from models.transformer_module import TransformerModule
 from utils.training_pipeline import TrainingPipeline
 import pickle
 import json
-
+from transformers import HfArgumentParser
+from utils.argument import TrainingArguments, DataArguments, TokenizerArguments,ModelArguments
+from loguru import logger
+logger = logger.bind(name="main")
+logger = logger.opt(colors=True)
 def get_vocab_size(token_vocab_path):
         # 读取 esperberto-vocab.json 文件
         with open(token_vocab_path, 'r', encoding='utf-8') as f:
@@ -19,42 +23,43 @@ def get_vocab_size(token_vocab_path):
     
 class Main:
     def __init__(self,training_args: TrainingArguments, data_args: DataArguments, tokenizaer_args: TokenizerArguments, model_args: ModelArguments):
-        self.train_args = training_args
+        self.training_args = training_args
         self.data_args = data_args
         self.tokenizer_args = tokenizaer_args
         self.model_args = model_args
         
         self.device = training_args.device
-        self.filepath = self.data_args.filepath
-        self.split_file_path = self.data_args.split_dict_filepath
-        self.sequence = self.train_args.sequence
-        self.hidden_dim = self.train_args.hidden_dim
-        self.mlm_probability = self.train_args.mlm_probability
-        self.batch_size = self.train_args.batch_size
-        self.epoch = self.train_args.epoch
-        self.pre_train = self.train_args.pre_train
-        self.pickle_path = self.train_args.pickle_path #"/root/autodl-tmp/pre_train/generate/data.pickle"
+        self.sequence = self.training_args.sequence
+        self.gnn_hidden_dim = self.training_args.gnn_hidden_dim
+        self.mlm_probability = self.training_args.mlm_probability
+        self.batch_size = self.training_args.batch_size
+        self.epoch = self.training_args.epoch
+        self.debug = self.training_args.debug
         
-        self.data_preprocessor = DataPreprocessor(  self.filepath, \
+        self.pickle_path = self.data_args.pickle_path #"/root/autodl-tmp/pre_train/generate/data.pickle"
+        self.raw_data_folder = self.data_args.raw_data_folder
+        
+        self.data_preprocessor = DataPreprocessor(  self.raw_data_folder, \
                                                     sequence = self.sequence,\
                                                     batch = self.batch_size,\
-                                                    tokenizaer_args=tokenizaer_args )
+                                                    tokenizaer_args=tokenizaer_args, debug=self.debug )
         
-        self.gnn_module = GNNModule(input_dim=self.sequence*self.hidden_dim, \
-                                    output_dim=self.sequence*self.hidden_dim, \
-                                    edge_dim = self.sequence*self.hidden_dim)
+        self.gnn_module = GNNModule(input_dim=self.sequence*self.gnn_hidden_dim, \
+                                    output_dim=self.sequence*self.gnn_hidden_dim, \
+                                    edge_dim = self.sequence*self.gnn_hidden_dim)
         
-        self.transformer_module = TransformerModule(output_dim = self.tokenizer_args.vocab_size )  # Example dimensions
+        self.transformer_module = TransformerModule(gpt_hidden_dim=training_args.gpt_hidden_dim, n_head=training_args.gpt_num_head)  # Example dimensions
         
-        self.training_pipeline = TrainingPipeline(  self.train_args, \
-                                                    vocab_size = self.tokenizer_args.vocab_size )  # 
+        self.training_pipeline = TrainingPipeline(  self.training_args, self.model_args, self.tokenizer_args )  # 
 
 
     def train_model(self):
-        if not os.path.exists(self.filepath):
+        if not os.path.exists(self.raw_data_folder):
             print("File path does not exist.")
             return None
             # Load and preprocess data
+        if self.debug:
+            self.pickle_path = f"{self.data_args.pickle_path}_debug"
         if not os.path.exists(self.pickle_path):
             graph_data = self.data_preprocessor.process_data()
             with open(self.pickle_path, 'wb') as handle:
@@ -62,15 +67,14 @@ class Main:
         else:
             with open(self.pickle_path, 'rb') as handle:
                 graph_data = pickle.load(handle)
-            print(1)
+            # print(1)
 
             # Validate processed data
             # Train the model
         model = self.training_pipeline.train(graph_data, self.gnn_module, self.transformer_module)
         return model
 
-from transformers import HfArgumentParser
-from utils.argument import TrainingArguments, DataArguments, TokenizerArguments,ModelArguments
+
 
 if __name__ == "__main__":
     # filepath = "/root/autodl-tmp/pre_train/generate/result"  # Placeholder path
@@ -80,12 +84,16 @@ if __name__ == "__main__":
     # token_merge = "esperberto-merges.txt"
     parser = HfArgumentParser((TrainingArguments, DataArguments, TokenizerArguments,ModelArguments))
     training_args, data_args, tokenizaer_args, model_args = parser.parse_args_into_dataclasses()
-    
+    logger.info("Training arguments: {}", training_args)
+    logger.info("Data arguments: {}", data_args)
+    logger.info("Tokenizer arguments: {}", tokenizaer_args)
+    logger.info("Model arguments: {}", model_args)
     
     training_args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    logger.info("Device: {}", training_args.device)
     
-    tokenizaer_args.token_vocab = "{}/esperberto-vocab.json".format(tokenizaer_args.tokenizer_dir)
-    tokenizaer_args.token_merge = "{}/esperberto-merges.txt".format(tokenizaer_args.tokenizer_dir)
+    tokenizaer_args.token_vocab = "{}/{}".format(tokenizaer_args.tokenizer_dir, tokenizaer_args.token_vocab)
+    tokenizaer_args.token_merge = "{}/{}".format(tokenizaer_args.tokenizer_dir, tokenizaer_args.token_merge)
     tokenizaer_args.vocab_size = get_vocab_size(tokenizaer_args.token_vocab)
     
     model_args.gnn_model_path = "{}/gnn_model.pth".format(model_args.output_dir)
